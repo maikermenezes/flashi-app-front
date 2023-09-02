116cb08751f43a75d4d2e6918c86a460fddd600a
Maike Menezes <maikermenezes@gmail.com>
2023-09-02 18:02:51 -0300

    feature(card flip): adding card flip animation

--- src/components/card/Card.component.tsx
+++ src/components/card/Card.component.tsx
@@ -1,8 +1,8 @@
 import React, { createElement, useState } from 'react';
 import { injectClassNames } from 'utils/css';
 import styles from './Card.module.scss';
-
-import { useGPTRequest } from 'hooks/api';
+import { GrSync } from "react-icons/gr";
+import { HiSpeakerWave } from "react-icons/hi2";
 
 const { loader } = styles;
 
@@ -22,22 +22,41 @@ const Card = (props: CardProps): JSX.Element => {
     translation
   } = props;
 
-  const [query, setQuery] = useState('');
-
-  const  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setQuery(event.target.value);
-  }
-
-  const handleSubmit = () => {
-    console.log("Query: ", query);
-  }
+  const [flip, setFlip] = useState(true);
   
   const className = injectClassNames( argClassName);
 
   return (
     <div className={styles.externalContainer}>
         <div className={`${styles.cardContainer} ${styles.gap}`}>
-            <img className={styles.imageStyling} src="https://source.unsplash.com/yWG-ndhxvqY" alt="imagem gerada" />
+
+            <div className={styles.divCard}>
+                <img className={`${styles.imageStyling} ${flip? styles.flipCard : '' }`} src="https://source.unsplash.com/yWG-ndhxvqY" alt="imagem gerada" />
+                <div className={`${styles.cardBack} ${!flip? styles.flipCard : '' }`}>
+                    <div className={styles.cardBackPhrase}>
+                        <span>She is cutting some herbs</span>
+                        <span>Ela está cortando ervas</span>
+                    </div>
+                </div> 
+            </div>
+
+            <span className={styles.iconSpeaker}>
+                <HiSpeakerWave size={25} />
+            </span>
+
+            <div className={styles.turnContainer}>
+                <span onClick={() => setFlip(!flip)}>
+                    <GrSync size={40} />
+                    <span>VIRAR</span>
+                </span>
+            </div>
+
+             <div className={styles.buttonContainer}>
+                <button>
+                    <span>PRÓXIMO CARD</span>
+                </button>
+             </div>
+
         </div>
     </div>
 
