10a417244d3f41dd9d34c3ffa573357f4a318211
Maike Menezes <maikermenezes@gmail.com>
2023-09-24 13:58:53 -0300

    fix(deploy): using no ssr component

--- src/pages/card.tsx
+++ src/pages/card.tsx
@@ -1,1 +1,1 @@
-export { default } from "routes/card";
+export { default } from "components/card";
--- src/pages/export.tsx
+++ src/pages/export.tsx
@@ -1,1 +1,1 @@
-export { default } from "routes/export";
+export { default } from "components/export";
